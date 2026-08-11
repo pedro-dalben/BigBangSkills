6ee543c05e05134f129695f7a321b952a315e300
pedro dalben <pedrodalbenmorais@gmail.com>
2026-08-11 13:11:38 -0300

    fix(fabric): ignore no-op teleports

--- fabric/src/main/java/com/bigbangcraft/bigbangskills/fabric/FabricBootstrap.java
+++ fabric/src/main/java/com/bigbangcraft/bigbangskills/fabric/FabricBootstrap.java
@@ -780,9 +780,10 @@ public final class FabricBootstrap implements ModInitializer {
 
     public static void beginSmeltingXp(net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity furnace) { CURRENT_SMELTING_FURNACE.set(furnace); }
     public static void endSmeltingXp() { CURRENT_SMELTING_FURNACE.remove(); }
-    public static void recordTeleport(ServerPlayer player) {
+    public static void recordTeleport(ServerPlayer player, net.minecraft.server.level.ServerLevel level, double x, double y, double z) {
         var instance = INSTANCE;
-        if (instance != null && instance.formulas.value("acrobatics.xp_after_teleport_cooldown_seconds") > 0) {
+        if (instance != null && (player.level().dimension() != level.dimension() || player.position().distanceToSqr(x, y, z) > 0.000001)
+                && instance.formulas.value("acrobatics.xp_after_teleport_cooldown_seconds") > 0) {
             instance.acrobaticsTeleportCooldowns.put(player.getUUID(), System.currentTimeMillis() + (long) (instance.formulas.value("acrobatics.xp_after_teleport_cooldown_seconds") * 1000));
         }
     }
--- fabric/src/main/java/com/bigbangcraft/bigbangskills/fabric/mixin/ServerPlayerMixin.java
+++ fabric/src/main/java/com/bigbangcraft/bigbangskills/fabric/mixin/ServerPlayerMixin.java
@@ -12,6 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class ServerPlayerMixin {
     @Inject(method = "teleportTo", at = @At("HEAD"))
     private void bigbangskills$recordTeleport(ServerLevel level, double x, double y, double z, float yRot, float xRot, CallbackInfo callback) {
-        FabricBootstrap.recordTeleport((ServerPlayer) (Object) this);
+        FabricBootstrap.recordTeleport((ServerPlayer) (Object) this, level, x, y, z);
     }
 }
